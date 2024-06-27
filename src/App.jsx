1dc6f634716ed8eb562693451d1dcbd558acca0f
dkaliazin <dkaliazin@gmail.com>
2024-06-27 19:22:32 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import Feedback from './Feedback';
 import Options from './Options';
 import Notification from './Notification';
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  })
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("saved-feedbacks");
+    return savedFeedback ?
+    JSON.parse(savedFeedback) : 
+    { good: 0, neutral: 0, bad: 0 };
+  });
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
@@ -27,14 +28,6 @@ function App() {
     }));
   }
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-
-  const [obj, setObj] = useState(() => {
-    const savedObj = localStorage.getItem("saved-feedbacks");
-    if (savedObj !== null) {
-      return JSON.parse(savedObj)
-    }
-    return{}
-  })
 
   useEffect(() => {
     localStorage.setItem("saved-feedbacks", JSON.stringify(feedback));
